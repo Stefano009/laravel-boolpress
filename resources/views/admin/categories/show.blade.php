da3b0ecae1d1a4a8ad78dcc4520da1987d9f5f76
Stefano009 <stefano.funaro@gmail.com>
2021-11-08 19:33:18 +0100

    Bonus-done

--- resources/views/admin/categories/show.blade.php
+++ resources/views/admin/categories/show.blade.php
@@ -11,13 +11,17 @@
             <div class="col-12">
                 <h2>lista dei post</h2>
                 <ul>
-                    @foreach ($category->posts as $post)
+                    @forelse ($category->posts as $post)
                     <li>
                         <a href="{{route('admin.posts.show', $post['slug'])}}">
+                            {{-- i use the slug 'cause i passed the slug in the post controller show if i want the id i need to change the controller show  --}}
+
                             {{$post->title}}
                         </a>    
-                    </li>                        
-                    @endforeach
+                    </li>     
+                    @empty
+                        <h3>-- Nessun post da visualizzare --</h3>                  
+                    @endforelse
                 </ul>
             </div>
         </div>
